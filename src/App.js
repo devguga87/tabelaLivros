9d3c22351c56472a071832aac09bd8141d8fe734
gustavo.seabra <devguga87@gmail.com>
2022-05-18 23:57:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import TabelaBody from './components/TabelaBody'
+import TabelaFoot from './components/TabelaFoot'
+import TabelaHead from './components/TabelaHead';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state={
+    livros:[
+      {
+        id:''
+      }
+    ]
+  }
+  render() {
+    return (
+      <table>
+        <TabelaHead />
+        <TabelaBody />
+        <TabelaFoot />
+      </table>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/components/TabelaHead.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TabelaHead = () => {
+  return (
+    <thead>
+      <tr>
+        <th colSpan='4'>Tabela de livros</th>
+      </tr>
+      <tr>
+        <th>ISBN</th>
+        <th>Titulo</th>
+        <th>Autor</th>
+        <th></th>
+      </tr>
+    </thead>
+  )
+}
+
+export default TabelaHead
